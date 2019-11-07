fedf3207c6fea93c923f9bb05b42e1b3fb9e3057
adriano <adriano.ramos@realizecfi.com.br>
2019-11-07 14:06:21 -0300

    Initial Commit

---
+++ FaceRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by Adriano Ramos on 07/11/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
